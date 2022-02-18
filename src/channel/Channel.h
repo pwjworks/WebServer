0f472e2d5e4316f3bff4f56f857f0348b18d7b29
pwjworks <13610513466@163.com>
2022-02-18 22:38:14 +0800

    add channel & timer

---
+++ src/channel/Channel.h
@@ -0,0 +1,96 @@
+#pragma once
+#include "Timer.h"
+#include <functional>
+#include <memory>
+#include <string>
+#include <sys/epoll.h>
+#include <unordered_map>
+
+class EventLoop;
+class HttpData;
+
+
+class Channel {
+private:
+  typedef std::function<void()> CallBack;
+  EventLoop *loop_;
+  int fd_;
+  __uint32_t events_;
+  // 接收的事件信号
+  __uint32_t revents_;
+  __uint32_t lastEvents_;
+
+  std::weak_ptr<HttpData> holder_;
+
+private:
+  int parse_URI();
+  int parse_Headers();
+  int analysisRequest();
+
+  CallBack readHandler_;
+  CallBack writeHandler_;
+  CallBack errorHandler_;
+  CallBack connHandler_;
+
+public:
+  Channel(EventLoop *loop);
+  Channel(EventLoop *loop, int fd);
+  ~Channel();
+
+
+  int getFd();
+  void setFd(int fd);
+
+  void setHolder(std::shared_ptr<HttpData> holder) { holder_ = holder; }
+
+  std::shared_ptr<HttpData> getHolder() {
+    std::shared_ptr<HttpData> ret(holder_.lock());
+    return ret;
+  }
+
+  void setReadHandler(CallBack &&readHandler) { readHandler_ = readHandler; }
+  void setWriteHandler(CallBack &&writeHandler) { writeHandler_ = writeHandler; }
+  void setErrorHandler(CallBack &&errorHandler) { errorHandler_ = errorHandler; }
+  void setConnHandler(CallBack &&connHandler) { connHandler_ = connHandler; }
+
+  /**
+   * @brief 根据revents_调用不同的回调函数
+   */
+  void handleEvents() {
+    events_ = 0;
+    if ((revents_ & EPOLLHUP) && !(revents_ & EPOLLIN)) {
+      events_ = 0;
+      return;
+    }
+    if (revents_ & EPOLLERR) {
+      if (errorHandler_) errorHandler_();
+      events_ = 0;
+      return;
+    }
+    if (revents_ & (EPOLLIN | EPOLLPRI | EPOLLRDHUP)) {
+      handleRead();
+    }
+    if (revents_ & EPOLLOUT) {
+      handleWrite();
+    }
+    handleConn();
+  }
+
+  void handleRead();
+  void handleWrite();
+  void handleError(int fd, int err_num, std::string short_msg);
+  void handleConn();
+
+  void setRevents(__uint32_t ev) { revents_ = ev; }
+
+  void setEvents(__uint32_t ev) { events_ = ev; }
+  __uint32_t &getEvents() { return events_; }
+
+  bool EqualAndUpdateLastEvents() {
+    bool ret = (lastEvents_ == events_);
+    lastEvents_ = events_;
+    return ret;
+  }
+
+  __uint32_t getLastEvents() { return lastEvents_; }
+};
