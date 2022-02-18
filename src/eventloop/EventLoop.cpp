3b67b340ef92175271fb9e4ee1e53090180512d5
pwjworks <13610513466@163.com>
2022-02-18 22:38:33 +0800

    modify eventloop

--- src/eventloop/EventLoop.cpp
+++ src/eventloop/EventLoop.cpp
@@ -2,22 +2,29 @@
 // Created by pwjworks on 2022/2/11.
 //
 
-#include "Eventloop.h"
+#include "EventLoop.h"
 #include <iostream>
 #include <sys/epoll.h>
 #include <sys/eventfd.h>
 
-void Eventloop::runInLoop(Functor &&cb) const {
+using namespace std;
+
+void EventLoop::runInLoop(Functor &&cb) {
   if (isInLoopThread())
     cb();
   else
     queueInLoop(std::move(cb));
 }
 
-void Eventloop::queueInLoop(std::function<void()> &&cb) {
+void EventLoop::queueInLoop(std::function<void()> &&cb) {
   {
-    MutexLockGuard lock(mutex_);
+    lock_guard<mutex> lck(mutex_);
     pendingFunctors_.emplace_back(std::move(cb));
   }
   if (!isInLoopThread() || callingPendingFunctors_) wakeup();
+}
+
+void EventLoop::wakeup() {
+  uint64_t one = 1;
+  ssize_t n =
 }
--- src/eventloop/EventLoop.h
+++ src/eventloop/EventLoop.h
@@ -5,19 +5,22 @@
 #include "Epoller.h"
 #include <functional>
 #include <memory>
+#include <mutex>
 #include <thread>
 #include <vector>
 
-class Eventloop {
+class EventLoop {
 public:
   typedef std::function<void()> Functor;
-  Eventloop();
-  ~Eventloop();
+  EventLoop();
+  ~EventLoop();
   void loop();
   void quit();
   void runInLoop(Functor &&cb) const;
   bool isInLoopThread() const { return threadId_ == std::CurrentThread::tid(); }
   void queueInLoop(std::function<void()> &&cb);
+
+  void wakeup();
 
 private:
   bool looping_;
@@ -25,7 +28,7 @@ private:
 
 private:
   bool callingPendingFunctors_;
-  mutable MutexLock mutex_;
+  mutable std::mutex mutex_;
   const pid_t threadId_;
   std::vector<Functor> pendingFunctors_;
 };
