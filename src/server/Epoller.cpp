916a165b8cc81d4e2818431e3cbfede2f74a21ba
pwjworks <13610513466@163.com>
2022-02-19 13:46:41 +0800

    modify epoller

--- src/server/Epoller.cpp
+++ src/server/Epoller.cpp
@@ -14,40 +14,86 @@ Epoller::Epoller() : epollFd_(epoll_create1(EPOLL_CLOEXEC)), events_(EVENTSNUM) 
 }
 Epoller::~Epoller() = default;
 
-void Epoller::epoll_add(int fd, __uint32_t fd_events_, int timeout) const {
+void Epoller::epoll_add(const SP_Channel &request, int timeout) {
+  int fd = request->getFd();
+  if (timeout > 0) {
+    add_timer(request, timeout);
+    fd2http_[fd] = request->getHolder();
+  }
   struct epoll_event event {};
   event.data.fd = fd;
-  event.events = fd_events_;
+  event.events = request->getEvents();
+
+  request->EqualAndUpdateLastEvents();
+
+  fd2chan_[fd] = request;
   if (epoll_ctl(epollFd_, EPOLL_CTL_ADD, fd, &event) < 0) {
-    std::cout << "epoll add error!" << std::endl;
-  } else {
-    std::cout << "epoll add success!" << std::endl;
+    perror("epoll_add error");
+    fd2chan_[fd].reset();
   }
 }
 
-void Epoller::epoll_mod(int fd, __uint32_t fd_events_, int timeout) const {
-  struct epoll_event event {};
+void Epoller::epoll_mod(const SP_Channel &request, int timeout) {
+  if (timeout > 0) add_timer(request, timeout);
+  int fd = request->getFd();
+  if (!request->EqualAndUpdateLastEvents()) {
+    struct epoll_event event {};
+    event.data.fd = fd;
+    event.events = request->getEvents();
+    if (epoll_ctl(epollFd_, EPOLL_CTL_MOD, fd, &event) < 0) {
+      perror("epoll_mod error");
+      fd2chan_[fd].reset();
+    }
+  }
+}
+
+void Epoller::epoll_del(const SP_Channel &request) {
+  int fd = request->getFd();
+  struct epoll_event event;
   event.data.fd = fd;
-  event.events = fd_events_;
-  if (epoll_ctl(epollFd_, EPOLL_CTL_MOD, fd, &event) < 0) {
-    std::cout << "epoll mod error!";
+  event.events = request->getLastEvents();
+  if (epoll_ctl(epollFd_, EPOLL_CTL_DEL, fd, &event) < 0) {
+    perror("epoll_del error");
   }
+  fd2chan_[fd].reset();
+  fd2http_[fd].reset();
 }
 
-void Epoller::epoll_del(int fd) const {
-  if (epoll_ctl(epollFd_, EPOLL_CTL_DEL, fd, nullptr) < 0) {
-    std::cout << "epoll del error!";
-  }
-}
-
-int Epoller::poll() {
+std::vector<std::shared_ptr<Channel>> Epoller::poll() {
   while (true) {
     int event_count = epoll_wait(epollFd_, &*events_.begin(), events_.size(), EPOLLWAIT_TIME);
-    if (event_count < 0) std::cout << "epoll wait error";
+    if (event_count < 0) perror("epoll wait error");
+    std::vector<SP_Channel> req_data = getEventsRequest(event_count);
     if (event_count == 0) continue;
-    return event_count;
+    if (!req_data.empty()) return req_data;
   }
 }
 const std::vector<epoll_event> &Epoller::getEvents() const {
   return events_;
+}
+
+void Epoller::add_timer(SP_Channel request_data, int timeout) {
+  std::shared_ptr<HttpData> t = request_data->getHolder();
+  if (t)
+    timerManager_.addTimer(t, timeout);
+  // TODO logger
+}
+
+void Epoller::handleExpired() {
+  timerManager_.handleExpiredEvent();
+}
+
+std::vector<std::shared_ptr<Channel>> Epoller::getEventsRequest(int events_num) {
+  std::vector<SP_Channel> req_data;
+  for (int i = 0; i < events_num; i++) {
+    int fd = events_[i].data.fd;
+    SP_Channel cur_req = fd2chan_[fd];
+    if (cur_req) {
+      cur_req->setRevents(events_[i].events);
+      cur_req->setEvents(0);
+      req_data.emplace_back(cur_req);
+    }
+    // TODO logger
+    return req_data;
+  }
 }
--- src/server/Epoller.h
+++ src/server/Epoller.h
@@ -2,6 +2,8 @@
 // Created by pwjworks on 2022/2/8.
 //
 #pragma once
+#include "Timer.h"
+#include <Channel.h>
 #include <sys/epoll.h>
 #include <vector>
 
@@ -9,15 +11,22 @@ class Epoller {
 public:
   Epoller();
   ~Epoller();
-  void epoll_add(int fd, __uint32_t events_, int timeout) const;
-  void epoll_mod(int fd, __uint32_t fd_events_, int timeout) const;
-  void epoll_del(int fd) const;
-  int poll();
+  void epoll_add(const SP_Channel &request, int timeout);
+  void epoll_mod(const SP_Channel &request, int timeout);
+  void epoll_del(const SP_Channel &request);
+  std::vector<std::shared_ptr<Channel>> poll();
+  std::vector<std::shared_ptr<Channel>> getEventsRequest(int events_num);
+  void add_timer(std::shared_ptr<Channel> request_data, int timeout);
+  int getEpollFd() const { return epollFd_; }
+  void handleExpired();
 
 private:
   static const int MAXFDS = 100000;
   int epollFd_;
   std::vector<epoll_event> events_;
+  std::shared_ptr<Channel> fd2chan_[MAXFDS];
+  std::shared_ptr<HttpData> fd2http_[MAXFDS];
+  TimerManager timerManager_;
 
 public:
   const std::vector<epoll_event> &getEvents() const;
